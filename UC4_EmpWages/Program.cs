a78d7676349b4af47fb552edb59b6196fd9d24a3
YogitaDevi <yogita.gry@gmail.com>
2022-12-03 18:47:47 +0530

    UC4

---
+++ UC4_EmpWages/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UC4_EmpWages
+{
+    class Program
+    {  
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+        public const int empRatePerHour = 20;
+        public static void Main(string[] args)
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case isPartTime:
+                    empHrs = 4;
+                    break;
+                case isFullTime:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+
+        
+    }
+}
