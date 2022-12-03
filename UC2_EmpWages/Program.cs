32137c5b4d9826b47cbf6f289af372917215e5a7
YogitaDevi <yogita.gry@gmail.com>
2022-12-03 18:22:59 +0530

    UC2

---
+++ UC2_EmpWages/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC2_EmpWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            int empRatePerHour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}
