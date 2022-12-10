e093788b2f9191c22e0f9580ffc7d8f8dccfd03f
YogitaDevi <117684801+YogitaDevi@users.noreply.github.com>
2022-12-10 23:06:50 +0530

    Random_Attendance

---
+++ UC1_EmpWages/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UC1_EmpWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
