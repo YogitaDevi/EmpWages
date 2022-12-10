1c72ed4420e39815b96accf56a141a6a9e3fc7d7
YogitaDevi <117684801+YogitaDevi@users.noreply.github.com>
2022-12-10 23:14:11 +0530

    CalculateWages_PerMonth

---
+++ UC6_EmpWages/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UC6_EmpWages
+{
+    class Program
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+        public const int empRatePerHour = 20;
+        public const int numOfWorkingDays = 2;
+        public const int maxHrsInMonth = 10;
+        static void Main(string[] args)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while(totalEmpHrs<=maxHrsInMonth && totalWorkingDays<numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days : " + totalWorkingDays + "Emp Hr :" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        }
+    }
+}
